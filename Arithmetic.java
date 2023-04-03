4ad28a1939ad569621e7b70d8619b95db18d7885
prajwalbobe302 <127470337+prajwalbobe302@users.noreply.github.com>
2023-04-03 12:34:33 +0530

    Add files via upload

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,12 +1,19 @@
-package Arithmetic;
+package Inheritance;
+
+class Arithmeticin {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmeticin {
+    // Adder inherits the add method from Arithmetic
+}
 
 public class Arithmetic {
-	public static int add(int a, int b) {
-		return a+b;
-		
-	}
-public static void main(String[] args) {
-	int result = add(7, 5);
-	System.out.println("The sum is: "+result);
-}
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        int result = adder.add(30, 2);
+        System.out.println("The sum is: " + result);
+    }
 }
