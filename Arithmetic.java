52bf768fbf8bfa67c87fc46ceeaa026ba860b5a4
prajwalbobe302 <127470337+prajwalbobe302@users.noreply.github.com>
2023-04-03 12:15:50 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+package Arithmetic;
+
+public class Arithmetic {
+	public static int add(int a, int b) {
+		return a+b;
+		
+	}
+public static void main(String[] args) {
+	int result = add(7, 5);
+	System.out.println("The sum is: "+result);
+}
+}
